9a5ad20f5c9ec7980dce2377999f270238554990
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-28 15:14:21 +0200

    created a working React App

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+class MyFlixApplication extends React.Component {
+    render() {
+        return (
+            <div className="my-Flix">
+                <div>Good morning</div>
+            </div>
+        );
+    }
+}
+
+const container = document.getElementsByClassName('app-container')[0];
+ReactDOM.render(React.createElement(MyFlixApplication), container);
