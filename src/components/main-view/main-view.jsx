d0456bdba22af5b7b85280e5e3717a103516b45b
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-06-28 17:16:13 +0200

    created main-view component

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+export class MainView extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            movies: [
+                { _id: 1, Title: 'Inception', Description: '"A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', ImagePath: '"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX67_CR0,0,67,98_AL_.jpg' },
+                { _id: 2, Title: 'The Shawshank Redemption', Description: 'Twi imprisoned men bond over a number of years,finding solace and eventual redemption throughacts of common decency.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEt0DM1ZmR1YWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@. V1 UX67 CR0,0,67,98 AL_.jpg' },
+                { _id: 3, Title: '12 Angry Men', Description: 'The jury in a New York City murder trial is frustrated by a single member whose skeptical caution forces them to more carefully consider the evidence before jumping to a hasty verdict.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_UX67_CR0,0,67,98_AL_.jpg' }
+            ],
+        }
+    }
+    setSelectedMovie(newSelectedMovie) {
+        this.setState({
+            selectedMovie: newSelectedMovie
+        });
+    }
+    render() {
+        const { movies, selectedMovie } = this.state;
+
+        if (movies.length === 0)
+            return <div className="main-view">The list is empty!</div>;
+        return (
+            <div className="main-view">
+                {selectedMovie
+                    ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                    : movies.map(movie => (
+                        <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+                    ))
+                }
+            </div>
+        );
+    }
+
+}
