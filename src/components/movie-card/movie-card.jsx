661b9ef60ca52ab4ef770afbc37f555d5098dc1e
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-07-02 19:51:46 +0200

    added addToFavouriteList method

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,13 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { MovieView } from '../movie-view/movie-view';
+import axios from 'axios';
+
+import { Link } from 'react-router-dom';
+
+import { Button, Card } from 'react-bootstrap/';
+
+import './movie-card.scss';
 
 export class MovieCard extends React.Component {
+
+    addToFavoriteList(movieId) {
+        const currentUser = localStorage.getItem('users');
+        const token = localStorage.getItem('token');
+        axios.put(`https://myflix-movies-heroku.herokuapp.com/users/${currentUser}/movies/${movieId}`,
+            {},
+            {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+            .then((response) => {
+                console.log(response.data)
+                alert(`The movie was successfully add to your list.`)
+            }).
+            catch(error => console.error(error))
+    }
+
     render() {
-        const { movie, onMovieClick } = this.props;
+        const { movie } = this.props;
+
         return (
-            <div className="movie-card" onClick={() => onMovieClick(movie)}>{movie.Title}</div>
-        );
+            <Card id="movie-card">
+                <Link to={`/movies/${movie._id}`}>
+                    <Card.Img variant="top" src={movie.ImagePath} />
+                </Link>
+                <Card.Body>
+                    <Card.Title id="card-title">{movie.Title}</Card.Title>
+                    <Card.Text>{movie.Description}</Card.Text>
+                    <Link to={`/movies/${movie._id}`}>
+                        <Button className="button" variant="outline-primary" size="sm">Open</Button>
+                    </Link>
+                    <Button className="button ml-2" variant="outline-primary" size="sm" onClick={() => this.addToFavoriteList(movie._id)}>Add</Button>
+                </Card.Body>
+            </Card>
+        )
     }
 }
 
@@ -16,16 +51,15 @@ MovieCard.propTypes = {
         Title: PropTypes.string.isRequired,
         Description: PropTypes.string.isRequired,
         ImagePath: PropTypes.string.isRequired,
+        Director: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+            Birth: PropTypes.number.isRequired,
+            Death: PropTypes.number.isRequired,
+        }),
         Genre: PropTypes.shape({
             Name: PropTypes.string.isRequired,
-            Description: PropTypes.string.isRequired,
-        }),
-        Director: PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            Birth: PropTypes.string.isRequired,
-            Bio: PropTypes.string.isRequired,
-        }),
-        Feature: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired
+        })
     }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
 };
