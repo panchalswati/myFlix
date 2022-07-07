d358b9b6ea3f438039c89af85d46faf2fbe13a0d
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-07-07 11:18:30 +0200

    changed Button variant to "link"

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -36,10 +36,10 @@ export class MovieView extends React.Component {
                     <Col className="value">{movie.Description}</Col>
                 </Row>
                 <Link to={`/directors/${movie.Director.Name}`}>
-                    <Button className="d-block mt-3" variant="info">Director</Button>
+                    <Button className="d-block mt-3" variant="link">Director</Button>
                 </Link>
                 <Link to={`/genres/${movie.Genre.Name}`}>
-                    <Button className="d-block mt-3" variant="info">Genre</Button>
+                    <Button className="d-block mt-3" variant="link">Genre</Button>
                 </Link>
                 <Button className="d-block mt-3" onClick={() => { onBackClick(null); }} variant="warning">Back</Button>
             </Container>
