1bb138d66becd8924540655a0801ec0d39f0762c
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-07-29 14:16:15 +0200

    updated Container of the view

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,43 +1,57 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 
-import { Button, Col, Container, Row, Col } from 'react-bootstrap';
-
-import './director-view.scss';
+import "./director-view.scss";
 
 export class DirectorView extends React.Component {
     render() {
+
         const { director, onBackClick } = this.props;
 
-        return (
+        if (director?.Death === undefined) return (
             <Container className="director-view">
-                <Row>
-                    <Col className="label">Director: </Col>
-                    <Col className="value">{director.Name}</Col>
+                return <Row>
+                    <Col className="left-col">Director: </Col>
+                    <Col className="right-col">{director?.Name + ' (born ' + director?.Birth + ')'}</Col>
                 </Row>
                 <Row className="mt-3">
-                    <Col className="label">Bio: </Col>
-                    <Col className="value">{director.Bio}</Col>
+                    <Col className="left-col">Bio: </Col>
+                    <Col className="right-col">{director?.Bio}</Col>
                 </Row>
                 <Row className="mt-3">
-                    <Col className="label">Birth: </Col>
-                    <Col className="value">{director.Birth}</Col>
+                    <Col className="left-col"></Col>
+                    <Col className="right-col">
+                        <Button
+                            className="d-block mt-3"
+                            onClick={() => { onBackClick(null); }}
+                        >Back
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+        )
+
+        if (director.Death !== undefined) return (
+            <Container className="director-view">
+                return <Row>
+                    <Col className="left-col">Director: </Col>
+                    <Col className="right-col">{director?.Name + ' (' + director?.Birth + '-' + director?.Death + ')'}</Col>
                 </Row>
                 <Row className="mt-3">
-                    <Col className="label">Birth: </Col>
-                    <Col className="value">{director.Death}</Col>
+                    <Col className="left-col">Bio: </Col>
+                    <Col className="right-col">{director?.Bio}</Col>
                 </Row>
-                <Button className="d-block mt-3" onClick={() => { onBackClick(null); }} variant="warning">Back</Button>
+                <Row className="mt-3">
+                    <Col className="left-col"></Col>
+                    <Col className="right-col">
+                        <Button
+                            className="d-block mt-3"
+                            onClick={() => { onBackClick(null); }}
+                        >Back
+                        </Button>
+                    </Col>
+                </Row>
             </Container>
         )
     }
 }
-
-DirectorView.propTypes = {
-    director: PropTypes.shape({
-        Name: PropTypes.string.isRequired,
-        Bio: PropTypes.string.isRequired,
-        Birth: PropTypes.string.isRequired,
-        Death: PropTypes.string.isRequired,
-    }).isRequired
-};
