136c36843963c71c8a88d1f1e35a7751deae21e2
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-07-29 14:17:01 +0200

    updated Container of the view

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { Button, Container, Row, Col } from 'react-bootstrap';
 
-import { Button, Col, Container, Row } from 'react-bootstrap';
-
-import './genre-view.scss';
+import "./genre-view.scss";
 
 export class GenreView extends React.Component {
     render() {
         const { genre, onBackClick } = this.props;
 
         return (
-            <Container className="director-view">
+            <Container className="mt-3 genre-view">
                 <Row>
-                    <Col className="label">Genre: </Col>
-                    <Col className="value">{genre.Name}</Col>
+                    <Col className="left-col">Genre: </Col>
+                    <Col className="right-col">{genre?.Name}</Col>
                 </Row>
                 <Row className="mt-3">
-                    <Col className="label">Description: </Col>
-                    <Col className="value">{genre.Description}</Col>
+                    <Col className="left-col">Description: </Col>
+                    <Col className="right-col">{genre?.Description}</Col>
                 </Row>
-                <Button className="d-block mt-3" onClick={() => { onBackClick(null); }} variant="warning">Back</Button>
+                <Row className="mt-3">
+                    <Col className="left-col"></Col>
+                    <Col className="right-col">
+                        <Button
+                            className="d-block mt-3"
+                            onClick={() => { onBackClick(null); }}
+                        >Back
+                        </Button>
+                    </Col>
+                </Row>
             </Container>
         )
     }
 }
-
-GenreView.propTypes = {
-    genre: PropTypes.shape({
-        Name: PropTypes.string.isRequired,
-        Description: PropTypes.string.isRequired
-    }).isRequired
-};
