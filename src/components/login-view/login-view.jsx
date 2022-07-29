595caaf04509eb690bb5ba7b06879ab192216a01
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-07-29 14:18:09 +0200

    added connect from react-redux package

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,43 +1,49 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import { Form, Button, Card, Container, Col, Row, CardGroup } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-import { Button, Col, Container, Form, Row } from 'react-bootstrap/';
+import { connect } from 'react-redux';
+import { setUser } from '../../actions/actions';
 
 import './login-view.scss';
 
 export function LoginView(props) {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+
     const [usernameErr, setUsernameErr] = useState('');
     const [passwordErr, setPasswordErr] = useState('');
 
-    // validate user inputs
     const validate = () => {
         let isReq = true;
         if (!username) {
             setUsernameErr('Username required');
             isReq = false;
-        } else if (username.length < 2) {
-            setUsernameErr('Username must be at least 2 characters long');
+        } else if (username.length < 5) {
+            setUsernameErr('Username must be at least 5 characters long');
             isReq = false;
         }
+
         if (!password) {
-            setPasswordErr('Password required');
+            setPasswordErr('Password Required');
             isReq = false;
         } else if (password.length < 6) {
             setPasswordErr('Password must be at least 6 characters long');
             isReq = false;
         }
+
         return isReq;
     }
 
+    //Requests server for authentication
+    //then calls props.onLoggedIn(username)
     const handleSubmit = (e) => {
         e.preventDefault();
         const isReq = validate();
+
         if (isReq) {
-            // Send a request to the server for authentication
             axios.post('https://myflix-movies-heroku.herokuapp.com/login', {
                 Username: username,
                 Password: password
@@ -47,44 +53,48 @@ export function LoginView(props) {
                     props.onLoggedIn(data);
                 })
                 .catch(e => {
-                    console.log('No such user')
+                    alert("That did not work. Please try again.")
+                    console.log('no such user')
                 });
         }
     };
 
     return (
-        <Container id="login-form">
-            <Row className="justify-content-center">
-                <Col sm="10" md="8" lg="6">
-                    <Form>
-                        <Form.Group controlId="formUsername">
-                            <Form.Label>Username:</Form.Label>
-                            <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Enter username" />
-                            {/* display validation error */}
-                            {usernameErr && <p>{usernameErr}</p>}
-                        </Form.Group>
-                        <Form.Group controlId="formPassword" className="mt-3">
-                            <Form.Label>Password:</Form.Label>
-                            <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
-                            {/* display validation error */}
-                            {passwordErr && <p>{passwordErr}</p>}
-                        </Form.Group>
-                        <Row className="mt-3 justify-content-start">
-                            <Col sm="10" md="8" lg="6">
-                                <Button variant="warning" type="submit" onClick={handleSubmit}>Login</Button>
-                            </Col>
-                        </Row>
-                    </Form>
-                </Col>
-            </Row>
+        <Container className="login-form">
+            <Form className="login-form bg-col lining">
+                <Form.Group className="mb-4" controlId="formUsername">
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+                    {/* code added here to display validation error */}
+                    {usernameErr && <p>{usernameErr}</p>}
+                </Form.Group>
+
+                <Form.Group className="mb-5" controlId="formPassword">
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                    {/* code added here to display validation error */}
+                    {passwordErr && <p>{passwordErr}</p>}
+                </Form.Group>
+                <Button type="submit" onClick={handleSubmit}>
+                    Log In
+                </Button>
+            </Form>
         </Container>
-    );
+    )
 }
 
 LoginView.propTypes = {
     user: PropTypes.shape({
         Username: PropTypes.string.isRequired,
-        Password: PropTypes.string.isRequired,
+        Password: PropTypes.string.isRequired
     }),
     onLoggedIn: PropTypes.func.isRequired
 }
+
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    };
+}
+
+export default connect(mapStateToProps, { setUser })(LoginView);
