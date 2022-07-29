6ba1ce49bd5dd3dcb4a22b58c120e336aa6e1d7b
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-07-29 14:15:34 +0200

    added actions for movies, user views

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,16 +1,41 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
-export const SET_USERS = 'SET_USERS';
+export const SET_USER = 'SET_USER';
+export const ADD_FAVMOVIE = 'ADD_FAVMOVIE';
+export const REM_FAVMOVIE = 'REM_FAVMOVIE';
 
 
 export function setMovies(value) {
-    return { type: SET_MOVIES, value };
-}
-
-export function setUsers(value) {
-    return { type: SET_USERS, value };
+    return {
+        type: SET_MOVIES,
+        value
+    };
 }
 
 export function setFilter(value) {
-    return { type: SET_FILTER, value };
+    return {
+        type: SET_FILTER,
+        value
+    };
+}
+
+export function setUser(user) {
+    return {
+        type: SET_USER,
+        user: user?.Username
+    };
+}
+
+export function addFavMovie(value) {
+    return {
+        type: ADD_FAVMOVIE,
+        value
+    }
+}
+
+export function remFavMovie(value) {
+    return {
+        type: REM_FAVMOVIE,
+        value
+    };
 }
