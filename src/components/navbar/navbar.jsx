09770c02e56f7d6b1408e647d6a40b4c13bb6686
Swati Panchal <102805610+panchalswati@users.noreply.github.com>
2022-07-29 14:25:32 +0200

    updated to Menubar and return function

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,14 @@
-import React from "react";
-import { Navbar, Nav, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
+import React from 'react';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 
-export function Navbar() {
-    let user = localStorage.getItem("user");
+import { connect } from 'react-redux';
+import { setUser } from '../../actions/actions';
 
-    const handleLogOut = (e) => {
-        e.preventDefault();
+export function MenuBar({ user }) {
+    const onLoggedOut = () => {
         localStorage.clear();
         window.open("/", "_self");
-        props.onLoggedOut(user);
-    };
+    }
 
     const isAuth = () => {
         if (typeof window == "undefined") {
@@ -22,31 +19,39 @@ export function Navbar() {
         } else {
             return false;
         }
-    };
+    }
 
     return (
-
-        <Navbar className="main-nav" sticky="top" bg="dark" expand="lg" variant="dark">
+        <Navbar className="main-nav" sticky="top" bg="dark" expand="sm" variant="dark">
             <Container>
-                <Navbar.Brand as={Link} to="/">MyFlix</Navbar.Brand>
-
-                <Nav className="ml-auto">
-                    {isAuth() && (
-                        <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>
-                    )}
-                    {isAuth() && (
-                        <Button variant="link" onClick={handleLogOut}>Logout</Button>
-                    )}
-                    {!isAuth() && (
-                        <Nav.Link href="/login">Login</Nav.Link>
-                    )}
-                    {!isAuth() && (
-                        <Nav.Link href="/register">Register</Nav.Link>
-                    )}
-                </Nav>
-
+                <Navbar.Brand className="navbar-logo" href="/">myFlix</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                        {isAuth() && (
+                            <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>
+                        )}
+                        {isAuth() && (
+                            <Button variant="link" onClick={() => { onLoggedOut() }}>Logout</Button>
+                        )}
+                        {!isAuth() && (
+                            <Nav.Link href="/">Login</Nav.Link>
+                        )}
+                        {!isAuth() && (
+                            <Nav.Link href="/register">Register</Nav.Link>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
-
-    )
+    );
 }
+
+let mapStateToProps = state => {
+    return {
+        movies: state.movies,
+        user: state.user
+    };
+}
+
+export default connect(mapStateToProps, { setUser })(MenuBar);
